846ae42b3390d5ec1e2b70f00cf9a0baa9ecf51e
Zameer <zameer@ahex.co.in>
2019-01-31 18:55:45 +0530

    handling form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') signupForm: NgForm;
+  deafaultQuestion = 'pet';
+  answer = '';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm){
+  //   console.log('form: ', form);
+
+  // }
+
+  onSubmit(){
+    console.log(this.signupForm);
   }
 }
